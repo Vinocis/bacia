f9cf69dcd92884a67f8f0a67c482304a7412fe8e
Vinicius <vinicius.alexandre68@gmail.com>
2024-07-11 21:43:58 -0300

    Create customers and transactions factories

---
+++ test/support/factory.ex
@@ -0,0 +1,45 @@
+defmodule Bacia.Factory do
+  use ExMachina.Ecto, repo: Bacia.Repo
+  
+  alias Bacia.Bank.Models.Customer
+  alias Bacia.Bank.Models.Transaction
+
+  def customer_factory(attrs) do
+    attrs = handle_customer_password(attrs)
+
+    customer = %Customer{
+      name: "Jhon Doe",
+      cpf: Brcpfcnpj.cpf_generate(),
+      balance: %Money{amount: 0, currency: :BRL}
+    }
+
+    customer
+    |> merge_attributes(attrs)
+    |> evaluate_lazy_attributes()
+  end
+
+  defp handle_customer_password(attrs) do
+    password = Map.get(attrs, :password, gen_password(100..1000))
+    hashed_password = Bcrypt.hash_pwd_salt(password)
+
+    attrs
+    |> Map.put(:password, password)
+    |> Map.put(:password_hash, hashed_password)
+  end
+
+  defp gen_password(range) do
+    range
+    |> Enum.random()
+    |> Integer.to_string()
+  end
+
+  def transaction_factory(attrs) do
+    amount = Map.get(attrs, :amount, 100_00)
+
+    %Transaction{
+      amount: %Money{amount: amount, currency: :BRL},
+      sender: build(:customer),
+      receiver: build(:customer)
+    }
+  end
+end
