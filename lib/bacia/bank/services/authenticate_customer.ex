0c8ab9c1d6dcb9d0695ce31f5ab32bdc4776d834
Vinicius <vinicius.alexandre68@gmail.com>
2024-09-09 21:08:44 -0300

    Create AuthenticateCustomer service tests

--- lib/bacia/bank/services/authenticate_customer.ex
+++ lib/bacia/bank/services/authenticate_customer.ex
@@ -7,12 +7,15 @@ defmodule Bacia.Bank.Services.AuthenticateCustomer do
   @spec process(map) :: {:ok, term()} | {:error, term()}
   def process(%{"cpf" => cpf, "password" => password}) do
     with {:ok, customer} <- CustomerRepo.fetch_by(%{cpf: cpf}),
-         true <- Bcrypt.verify_pass(password, customer.password_hash),
+         :ok <- check_pass(password, customer.password_hash),
          {:ok, token, _claims} <- Guardian.encode_and_sign(customer) do
       {:ok, token}
-    else
-      false -> 
-        {:error, :unauthorized}
     end
+  end
+
+  defp check_pass(password, password_hash) do
+    if Bcrypt.verify_pass(password, password_hash),
+      do: :ok,
+      else: {:error, :unauthorized}
   end
 end
