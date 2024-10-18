5ff5d99ca2e791d8292012db03ad228cc3bf6efc
Vinicius <vinicius.alexandre68@gmail.com>
2024-10-17 22:15:40 -0300

    Creates Transaction Producer and Consumer

---
+++ lib/bacia/bank/producers/transaction.ex
@@ -0,0 +1,19 @@
+defmodule Bacia.Bank.Producers.Transaction do
+  use GenStage
+
+  alias Bacia.Bank.Services.HandleTransaction
+
+  def start_link() do
+    GenStage.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok), do: {:producer, :ok}
+
+  def handle_call(message, _from, state) do
+    {:noreply, [message], state}
+  end
+
+  def handle_demand(_demand, state) do
+    {:noreply, [], state}
+  end
+end
